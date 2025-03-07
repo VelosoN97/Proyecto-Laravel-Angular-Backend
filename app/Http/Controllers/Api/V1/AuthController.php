6515068f22dc33af9c298b9e6518a04f342a3064
VelosoN97 <nicolas.veloso0397@gmail.com>
2025-03-07 00:10:17 -0300

    Tercer commit - Creación Función Login

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -3,12 +3,27 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\V1\LoginRequest;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function login(): JsonResponse{
-        
+    public function login(LoginRequest $request): JsonResponse{
+        if(! Auth::attempt($request->validated())){
+            return response()->json([
+                'errors' => 'Credenciales incorrectas.'
+            ], 401);
+        }
+        $user = $request->user();
+        $userToken = $user->createToken('AppToken')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Se ha iniciado sesión correctamente!.',
+            'token' => $userToken,
+            'user' => $user
+        ]);
     }
 }
